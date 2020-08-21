2f8d2146c21bdb012b1d6b5a8a34fec1e74d6f01
izzudd <rahmatullohdaffaizzuddin1@gmail.com>
2020-08-21 20:04:39 +0700

    production state

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 	"sync"
 
 	_ "github.com/lib/pq"
@@ -25,19 +26,27 @@ func main() {
 	go startSessionCleanup(serverExit, sessionCleanerExit)
 
 	// if enter pressed
-	println("\nmain: Press enter key to shut down")
+	if os.Getenv("APP_STATE") != "PRODUCTION" {
+		println("\nmain: Press enter key to shut down")
+	}
 	println("main: Server started at", server.Addr, "if no error occured")
-	fmt.Scanln()
 
 	// shutdown
-	println("main: Server shutting down...")
-	sessionCleanerExit <- 0
-	err := server.Shutdown(context.TODO())
-	if err != nil {
-		panic(err)
-	}
+	if os.Getenv("APP_STATE") != "PRODUCTION" {
+		fmt.Scanln()
+		println("main: Server shutting down...")
+		sessionCleanerExit <- 0
+		err := server.Shutdown(context.TODO())
+		if err != nil {
+			panic(err)
+		}
 
-	// wait server to exit
-	serverExit.Wait()
-	println("main: Done exiting")
+		// wait server to exit
+		serverExit.Wait()
+		println("main: Done exiting")
+	} else {
+		println("Server Started")
+		for {
+		}
+	}
 }
