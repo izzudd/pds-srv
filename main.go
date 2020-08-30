4eee7ea398884fbd12b8aab5bae25d5d082680e7
izzudd <rahmatullohdaffaizzuddin1@gmail.com>
2020-08-30 15:44:24 +0700

    exit command

--- main.go
+++ main.go
@@ -25,11 +25,15 @@ func main() {
 	go startSessionCleanup(serverExit, sessionCleanerExit)
 
 	// if enter pressed
-	println("\nmain: Press enter key to shut down")
+	println("\nmain: Type exit then enter to shut down")
 	println("main: Server started at", server.Addr, "if no error occured")
-	fmt.Scanln()
 
-	println("main: Server shutting down...")
+	exit := ""
+	for exit != "exit" {
+		fmt.Scanln(&exit)
+	}
+
+	println("\nmain: Server shutting down...")
 	sessionCleanerExit <- 0
 	err := server.Shutdown(context.TODO())
 	if err != nil {
